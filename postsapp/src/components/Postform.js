ed2a9c721a21b60b6d6c3f50190110beac23697c
Vignesh Shenoy <vignesh.shenoy@propertyloop.co.uk>
2022-11-03 02:57:19 +0530

    Integrated Mock api

---
+++ postsapp/src/components/Postform.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createPost } from "../actions/postActions";
+
+class Postform extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: "",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: [e.target.value],
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body,
+    };
+
+    this.props.createPost(post);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title:</label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+          <div>
+            <label>Body:</label>
+            <br />
+            <input
+              type="text"
+              name="body"
+              onChange={this.onChange}
+              value={this.state.body}
+            />
+          </div>
+          <button type="submit">Add Post</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { createPost })(Postform);
---
+++ postsapp/src/store.js
@@ -0,0 +1,16 @@
+//our actual data will be here in the store
+import {applyMiddleware, compose, createStore} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+
+const initialState = {}
+const middleware = [thunk]
+const store =  createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware)
+    )
+)
+
+export default store;
